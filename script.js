a2d3a848444545defc2557c0acb2ab0f53cd5bb6
akao2003 <x22g046@mitsuhashi.ac.jp>
2024-09-30 10:05:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const csvUrl = 'https://raw.githubusercontent.com/inagaki335/iwashirasu/main/test.csv';
-
 fetch(csvUrl)
     .then(response => {
         if (!response.ok) {
             throw new Error('ネットワークエラー: ' + response.statusText);
         }
-        return response.text(); // テキストとして取得
+        return response.text();
     })
     .then(csvText => {
-        console.log(csvText); // ここで取得したCSVデータをログに出力
+        console.log(csvText);  // ここで内容を確認
         document.getElementById('output').textContent = csvText; // テキストを表示
     })
     .catch(error => {

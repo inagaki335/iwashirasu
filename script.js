170c0990fa125e76030a2912de80611b0a5fe14d
akao2003 <x22g046@mitsuhashi.ac.jp>
2024-09-30 09:42:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // GitHubのCSVファイルのURLを設定
-const csvUrl = 'https://raw.githubusercontent.com/inagaki335/iwashirasu/main/test.csv';
+const xlsxUrl = 'https://raw.githubusercontent.com/inagaki335/iwashirasu/main/テストエクセルファイル.xlsx';
 
 // ページが読み込まれたときにCSVを取得
-fetch(csvUrl)
+fetch(xlsxUrl)
     .then(response => {
         if (!response.ok) {
             throw new Error('ネットワークエラー: ' + response.statusText);
         }
         return response.text(); // テキストとして取得
     })
-    .then(csvText => {
-        document.getElementById('output').textContent = csvText; // テキストを表示
+    .then(xlsxText => {
+        document.getElementById('output').textContent = xlsxText; // テキストを表示
     })
     .catch(error => {
         console.error('エラー:', error);

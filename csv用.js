37e166dd2d14168d2b116354a76a5a34950253bc
akao2003 <x22g046@mitsuhashi.ac.jp>
2024-10-08 11:02:23 +0900

    Update csv用.js

--- csv用.js
+++ csv用.js
@@ -45,7 +45,7 @@ fetch(csvUrl)
     });
 
 window.addEventListener('DOMContentLoaded', function() {
-    const imgPath = './蜂.jpg';
+    const imgPath = '蜂.jpg';
     const imgElement = document.getElementById('image1');
     imgElement.src = imgPath;
 });
